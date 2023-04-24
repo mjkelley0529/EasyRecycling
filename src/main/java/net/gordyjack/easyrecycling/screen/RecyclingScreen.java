02301aff93ec752436d5a3cb64e68c4e588e7099
gordyjack <xkelley0529@gmail.com>
2023-04-24 11:51:25 -0500

    Basic functionality and textures are complete. Ready for versioning and publishing.

--- src/main/java/net/gordyjack/easyrecycling/screen/RecyclingScreen.java
+++ src/main/java/net/gordyjack/easyrecycling/screen/RecyclingScreen.java
@@ -2,10 +2,12 @@ package net.gordyjack.easyrecycling.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.gordyjack.easyrecycling.EasyRecycling;
+import net.minecraft.client.gui.screen.ingame.GrindstoneScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.GrindstoneScreenHandler;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -34,6 +36,9 @@ public class RecyclingScreen extends HandledScreen<RecyclingScreenHandler> {
         RenderSystem.setShaderTexture(0, TEXTURE);
 
         drawTexture(matrices, xD2(), yD2(), 0, 0, backgroundWidth, backgroundHeight);
+        if (this.handler.getSlot(0).hasStack() && !this.handler.getSlot(1).hasStack()) {
+            drawTexture(matrices, xD2() + 92, yD2() + 31, this.backgroundWidth, 0, 28, 21);
+        }
     }
     @Override
     protected void init() {
